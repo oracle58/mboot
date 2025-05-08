5340d487d30300dcec37889125df1b4c976685ca
oracle58 <fohrmann.oliver@gmail.com>
2025-05-08 18:44:22 +0200

    Add breakpoints to gdb

--- gdb.sh
+++ gdb.sh
@@ -1,6 +1,10 @@
 gdb -q \
   -ex "add-symbol-file ./build/kernel.o 0x100000" \
   -ex "break kmain" \
-  -ex "break kernel.c:12" \
+  -ex "break kernel.c:14" \
+  -ex "break kernel.c:17" \
+  -ex "break kernel.c:18" \
+  -ex "break kernel.c:26" \
+  -ex "break kernel.c:27" \
   -ex "target remote localhost:1234" \
   -ex "c"
