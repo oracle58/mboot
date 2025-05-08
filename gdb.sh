a64124db629dca1dfceea3cf8cf6b7f7bf0c00c2
oracle58 <fohrmann.oliver@gmail.com>
2025-05-09 00:07:56 +0200

    logged gdb batch

--- gdb.sh
+++ gdb.sh
@@ -1,8 +1,15 @@
-gdb -q \
-  -ex "set architecture i386:intel"\
-  -ex "target remote localhost:1234" \
-  -ex "add-symbol-file ./build/kernel.o 0x100000" \
-  -ex "break kmain" \
-  -ex "break kernel.c:6" \
-  -ex "break kernel.c:7" \
-  -ex "c"
+gdb -q --batch \
+    -ex "set pagination off" \
+    -ex "set architecture i386:intel" \
+    -ex "target remote localhost:1234" \
+    -ex "add-symbol-file ./build/kernel.o 0x100000" \
+    -ex 'echo "\nMemory at 0x7E00 (kernel source):\n"' \
+    -ex "x/32b 0x7E00" \
+    -ex 'echo "\nMemory at 0x100000 (kernel destination):\n"' \
+    -ex "x/32b 0x100000" \
+    -ex "break *0x100000" \
+    -ex "break kmain" \
+    -ex "c" \
+    -ex "x/8x 0xb8000" \
+    -ex "info registers" \
+  > gdb.log 2>&1
